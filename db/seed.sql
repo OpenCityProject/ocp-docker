14b465ea84ebfabbbfbab8189f1f2737bf44b178
Eddie Chiang <eddiecjc@gmail.com>
2016-12-19 22:28:01 +1300

    Adding POI approval status and Moderator Email verification status
    Update the database design to fulfil the following use cases:
    - Approval/Rejection of Events
    - Moderator Account Creation: Require email verification

--- db/seed.sql
+++ db/seed.sql
@@ -9,12 +9,24 @@ INSERT INTO public.participant_type(participant_type_id, participant_type_name) 
     (2, 'Room'), 
     (3, 'Resource'),
     (4, 'Group');
-    
+
+INSERT INTO public.person_state(person_state_id, person_state_name) VALUES 
+    (1, 'Active'),
+    (2, 'Deleted'),
+    (3, 'Disabled'),
+    (4, 'EmailNotVerified'),
+    (5, 'Locked');
+
 INSERT INTO public.person_type(person_type_id, person_type_name) VALUES 
-	(1, 'Admin'),
+    (1, 'Admin'),
     (2, 'Moderator'),
     (3, 'End User');
 
+INSERT INTO public.poi_state(poi_state_id, poi_state_name) VALUES 
+    (1, 'Approved'),
+    (2, 'Declined'),
+    (3, 'Deleted'),
+    (4, 'Submitted');
 
 INSERT INTO public.recurrence_frequency(recurrence_frequency_id, recurrence_frequency_name) VALUES 
 	(1, 'Daily'),
@@ -31,10 +43,6 @@ INSERT INTO public.weekday(weekday_id, weekday_name) VALUES
     (6, 'Saturday'),
     (7, 'Sunday');
 
-INSERT INTO public.person_state(person_state_id, person_state_name) VALUES 
-	(1, 'Active'),
-    (2, 'Disabled'),
-    (3, 'Deleted'),
-    (4, 'Locked');
+
  
 
