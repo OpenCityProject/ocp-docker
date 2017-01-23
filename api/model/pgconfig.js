a431008d034334994d77a98eca13aac5df25eeaf
Eddie Chiang <eddiecjc@gmail.com>
2017-01-23 22:53:46 +1300

    Remove api folder
    As there are currently two Node.js apps:
    1. api
    2. api-def
    
    We have chosen to continue with api-def which is generated by swagger.
    Thus removing api folder to avoid confusion.

--- api/model/pgconfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  client: 'pg',
-  connection: {
-    host : 'db',
-    user : process.env.POSTGRES_USER,
-    password : process.env.POSTGRES_PASSWORD,
-    database : process.env.POSTGRES_DB
-  }
-}
