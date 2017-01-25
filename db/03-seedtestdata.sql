0a346a1352d957e9411285bf875eb088335ca79c
Eddie Chiang <eddiecjc@gmail.com>
2017-01-25 06:26:49 +0000

    Add test data seed script

---
+++ db/03-seedtestdata.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.person(person_name, phone_number, email, person_type_id, oauth_provider_id, oauth_token, oauth_token_expiration, person_state_id) VALUES 
+    ('Eddie', '01234567', 'eddie@example.com', 1, NULL, 'Abcdefghijklmno', '9999-9-9', 1),
+	('Elliot', '02234567', 'elliot@example.com', 1, NULL, 'Bbcdefghijklmno', '9999-9-9', 1);
