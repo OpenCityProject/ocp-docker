198ffceeb486f13afea5192060f9dbeff47ff203
Wesley Yep <wesleyyep@Wesleys-MacBook-Air.local>
2017-01-26 11:45:24 +1100

    Using knex to query all POIs from the postgres db

---
+++ api-def/node-server/model/pgdb.js
@@ -0,0 +1,11 @@
+const pgconfig = require('./pgconfig')
+const knex = require('knex')(pgconfig)
+
+module.exports = {
+  test(){
+    knex.select(knex.raw(1))
+  },
+  getPoi(){
+    return knex.select().from('poi');
+  }
+}
