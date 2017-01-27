6c0639019c2c4c17f4e425eaf694db35633d90a0
Wesley Yep <wesleyyep@hotmail.com>
2017-01-27 23:21:22 +1100

    PATCH

--- api-def/node-server/controllers/PointOfInterestService.js
+++ api-def/node-server/controllers/PointOfInterestService.js
@@ -72,18 +72,19 @@ exports.poiPATCH = function(args, res, next) {
    * poi Poi Point of Interest object (optional)
    * returns Success
    **/
-  var examples = {};
-  examples['application/json'] = {
-  "code" : 123,
-  "message" : "aeiou",
-  "object" : "{}"
-};
-  if (Object.keys(examples).length > 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  } else {
-    res.end();
-  }
+  console.log(args.poiId.value);
+  var poiDTO = args.poi.value;
+  var poi = { poi_id:  poiDTO.id, poi_name: poiDTO.name, location_title: "unknown", location_polygon: poiDTO.location_polygon, recurrence_rule_id: 12345,
+              start_date: poiDTO.start_date, end_date: poiDTO.end_date, poi_url: poiDTO.poi_url, poi_description: poiDTO.description,
+                poi_state_id: 1, who_added_patron_id: "009b4c56-e2c9-11e6-940b-6f54577f0d9d" }
+  const query = pgdb.updatePoi(args.poiId.value, poi);
+  query.then(response=>{
+    console.log(response);
+    res.end(JSON.stringify(response));
+  }).catch(err=>{
+    console.error('Error', err);
+    res.end(JSON.stringify(err));
+  })
 }
 
 exports.poiPOST = function(args, res, next) {
