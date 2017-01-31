21951f7222c051fbe2b1b036d41b7cd8d5f0b080
Eddie Chiang <eddiecjc@gmail.com>
2017-02-01 00:36:28 +1300

    Update POI methods
    - getPoi(lat, long)
    - getPoiState

--- api-def/node-server/model/poi.js
+++ api-def/node-server/model/poi.js
@@ -5,8 +5,26 @@ module.exports = {
 	test() {
 		knex.select(knex.raw(1))
 	},
-	getPoi() {
-		return knex.select().from('poi');
+	getPoi(lat, long) {
+		// return knex
+		// 	.select(
+		// 		'poi_id AS id',
+		// 		'poi_name AS name',
+		// 		'location_gps_coordinate AS gps_lat',
+		// 		'location_gps_coordinate AS gps_long',
+		// 		'location_polygon',
+		// 		'start_date',
+		// 		'end_date',
+		// 		'NULL AS opening_hours',
+		// 		'NULL AS is_all_day',
+		// 		'poi_url',
+		// 		'poi_description AS description',
+		// 		'NULL AS categories',
+		// 		'NULL AS tags')
+		// 	.from('poi');
+		return knex
+			.select()
+			.from('poi');
 	},
 	insertPoi(poi) {
 		console.log("poi is: ")
